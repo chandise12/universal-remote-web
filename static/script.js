accbb0ebbad334ea1fe97943e364226abe5e8824
Chandise <c6anders@uwaterloo.ca>
2026-09-07 19:21:36 -0400

    added css to remote + button pages

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 
-const emptyRemoteError = document.getElementById("error-empty-remote");
+const emptyCreateRemoteError = document.getElementById("error-empty-remote");
 
 const remoteForm = document.getElementById("create-remote");
 if(remoteForm){
@@ -23,7 +23,7 @@ if(remoteForm){
                 window.location.reload();
             } else {
                 console.error("Server error:", result);
-                emptyRemoteError.textContent = "Please enter a name for this remote!";
+                emptyCreateRemoteError.textContent = "Please enter a name for this remote!";
             }
 
             console.log('Success:', result);
@@ -55,6 +55,80 @@ if(remoteForm){
             }   
         });
     });
+
+
+    const renameRemote = document.querySelectorAll(".rename-remote");
+
+    renameRemote.forEach(button =>{
+
+        button.addEventListener("click", () => {
+            const listItem = button.closest("li");
+            const form = listItem.querySelector(".rename-remote-form");
+
+            button.hidden = true;
+            form.hidden = false;
+        });
+    });
+
+
+    const cancelRenameRemote = document.querySelectorAll(".cancel-rename-remote");
+
+    cancelRenameRemote.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            const listItem = button.closest("li");
+            const form = listItem.querySelector(".rename-remote-form");
+            const renameButton = listItem.querySelector(".rename-remote");
+            const error = form.querySelector(".error-empty-remote");
+
+            form.hidden = true;
+            renameButton.hidden = false;
+            error.textContent = "";
+        });
+    });
+
+
+    const renameRemoteForm = document.querySelectorAll(".rename-remote-form");
+
+    renameRemoteForm.forEach(form => {
+
+        form.addEventListener("submit", async (event) => {
+
+            event.preventDefault();
+
+            const remoteId = form.dataset.remoteId;
+
+            const formData = new FormData(form);
+            const data = Object.fromEntries(formData.entries());
+
+            const emptyRenameRemoteError = form.querySelector(".error-empty-remote");
+
+            try {
+
+                const response = await fetch(`/remotes/${remoteId}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                if (response.ok) {
+                    window.location.reload();
+                }else{
+                    emptyRenameRemoteError.textContent = "Please enter a name for this remote!";
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
+
+        });
+
+    });
+
+
 }
 
 
@@ -120,6 +194,112 @@ if(buttonForm){
                 console.error('Error:', error);
             }   
         });
+    });
+
+
+    const commandButtons = document.querySelectorAll(".command-button");
+
+    commandButtons.forEach(button => {
+        button.addEventListener("click", async () => {
+
+            const remoteId = button.dataset.remoteId;
+            const buttonId = button.dataset.buttonId;
+
+            try {
+                const response = await fetch(
+                    `/remotes/${remoteId}/buttons/${buttonId}/command`,
+                    {
+                        method: "POST"
+                    }
+                );
+
+                const result = await response.json();
+
+                if (response.ok) {
+                    console.log(result.status);
+                } else {
+                    console.error(result);
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
+        });
+    });
+
+
+
+
+    const renameButton = document.querySelectorAll(".rename-button");
+
+    renameButton.forEach(button =>{
+
+        button.addEventListener("click", () => {
+            const listItem = button.closest("li");
+            const form = listItem.querySelector(".rename-button-form");
+
+            button.hidden = true;
+            form.hidden = false;
+        });
+    });
+
+
+    const cancelRenameButton = document.querySelectorAll(".cancel-rename-button");
+
+    cancelRenameButton.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            const listItem = button.closest("li");
+            const form = listItem.querySelector(".rename-button-form");
+            const renameButton = listItem.querySelector(".rename-button");
+            const error = form.querySelector(".error-empty-button");
+
+            form.hidden = true;
+            renameButton.hidden = false;
+            error.textContent = "";
+        });
+    });
+
+
+    const renameButtonForm = document.querySelectorAll(".rename-button-form");
+
+    renameButtonForm.forEach(form => {
+
+        form.addEventListener("submit", async (event) => {
+
+            event.preventDefault();
+
+            const remoteId = form.dataset.remoteId;
+            const buttonId = form.dataset.buttonId;
+
+            const formData = new FormData(form);
+            const data = Object.fromEntries(formData.entries());
+
+            const emptyRenameRemoteError = form.querySelector(".error-empty-button");
+
+            try {
+
+                const response = await fetch(`/remotes/${remoteId}/buttons/${buttonId}`, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                if (response.ok) {
+                    window.location.reload();
+                }else{
+                    emptyRenameRemoteError.textContent = "Please enter a name for this remote!";
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
+
+        });
+
     });
 
 }
